3dd0e190f5d8b02e14bcf19b35f918469e3a3bf4
Rowan Richardson <rr.designweb@gmail.com>
2020-01-25 12:02:00 +1100

    Setup express server and moved frontend into client folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
-//pk_test_nnEL9T5XEhqsP8YcmxJKnIkh002t6y9a7T
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_nnEL9T5XEhqsP8YcmxJKnIkh002t6y9a7T";
+
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert("Succesful payment");
+      })
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
