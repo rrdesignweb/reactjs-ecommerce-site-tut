75e930615d8fc87e1eff2713099aa045eb9ddfd3
Rowan Richardson <rr.designweb@gmail.com>
2020-01-30 22:25:04 +1100

    Fix server.js

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,22 +1,11 @@
-import React, { Profiler } from "react";
+import React from "react";
 import { HomePageContainer } from "./homepage.styles";
 
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <HomePageContainer>
-    <Profiler
-      id="Directory"
-      onRender={(id, phase, actualDuration) => {
-        console.log({
-          id,
-          phase,
-          actualDuration
-        });
-      }}
-    >
-      <Directory />
-    </Profiler>
+    <Directory />
   </HomePageContainer>
 );
 
